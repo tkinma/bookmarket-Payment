97c210718eba8e555851d985b8293e41e530355f
tkinma <70673849+tkinma@users.noreply.github.com>
2020-11-04 21:44:45 +0900

    Update PaymentController.java

--- src/main/java/bookmarket/PaymentController.java
+++ src/main/java/bookmarket/PaymentController.java
@@ -1,13 +1,13 @@
 package bookmarket;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
+
+import java.util.Optional;
 
 @RestController
 public class PaymentController {
